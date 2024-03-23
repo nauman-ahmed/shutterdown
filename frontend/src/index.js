438446a2f207ec9ea0d7cca49dc792381a8e73fe
safdarAwaN786 <safdarawan2003@gmail.com>
2024-03-23 05:23:47 +0500

    ready to deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './config/golbal';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter , HashRouter} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { GoogleOAuthProvider } from '@react-oauth/google';
@@ -24,7 +23,3 @@ root.render(
   ,
   document.getElementById('root')
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
