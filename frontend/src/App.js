39f960bd6a4f3d60c103f68dc4abbb906570cda9
Nauman Ahmed <naumanahmed449@gmail.com>
2024-10-28 19:43:20 +0100

    Updated version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ import DeliverablesDeadline from "./screens/Deadlines";
 function App() {
   const location = useLocation();
   const currentUser =  Cookies.get("currentUser") && JSON.parse(Cookies.get("currentUser"));
-  const Version = "1.0.3"
+  const Version = "1.0.4"
 
   useEffect(() => {
     window.scrollTo(0, 0);
